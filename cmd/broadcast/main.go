950e5bc2535cff7fa6abeba6a4674512f7a6fb92
Sahil Patil <sp92535@gmail.com>
2026-06-08 17:18:44 +0000

    Added challenge #3a

---
+++ cmd/broadcast/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"slices"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	var mu sync.Mutex
+	arr := make([]float64, 0)
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		mu.Lock()
+		arr = append(arr, body["message"].(float64))
+		mu.Unlock()
+
+		body["type"] = "broadcast_ok"
+
+		delete(body, "message")
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "read_ok"
+		body["messages"] = slices.Clone(arr)
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+
+		delete(body, "topology")
+		return n.Reply(msg, body)
+	})
+
+	log.Println("Server running...")
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
