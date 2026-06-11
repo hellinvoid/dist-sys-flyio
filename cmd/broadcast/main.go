a78e7fc82856df773f22f14aa0a72db3908abc8c
Sahil Patil <sp92535@gmail.com>
2026-06-11 15:53:19 +0000

    Added #3b: broadcast multi node gossip

--- cmd/broadcast/main.go
+++ cmd/broadcast/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"log"
-	"slices"
 	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
-	n := maelstrom.NewNode()
+	var once sync.Once
 
 	var mu sync.Mutex
 	arr := make([]float64, 0)
+
+	n := maelstrom.NewNode()
+	next := make([]string, 0)
+
+	seenGossipUid := map[string]any{}
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -21,9 +26,13 @@ func main() {
 			return err
 		}
 
+		val := body["message"].(float64)
+
 		mu.Lock()
-		arr = append(arr, body["message"].(float64))
+		arr = append(arr, val)
 		mu.Unlock()
+
+		go SendNewGossip(n, next, val)
 
 		body["type"] = "broadcast_ok"
 
@@ -38,7 +47,11 @@ func main() {
 		}
 
 		body["type"] = "read_ok"
-		body["messages"] = slices.Clone(arr)
+		mu.Lock()
+		msgs := append([]float64(nil), arr...)
+		mu.Unlock()
+
+		body["messages"] = msgs
 
 		return n.Reply(msg, body)
 	})
@@ -49,14 +62,77 @@ func main() {
 			return err
 		}
 
+		isMsgFromNode := msg.Src[0] == 'n'
+
+		once.Do(func() {
+			mu.Lock()
+
+			neighbours := body["topology"].(map[string]any)[n.ID()].([]any)
+			for _, n := range neighbours {
+				next = append(next, n.(string))
+			}
+
+			for _, nxt := range next {
+				if msg.Src != nxt {
+					n.Send(nxt, body)
+				}
+			}
+			for _, val := range arr {
+				go SendNewGossip(n, next, val)
+			}
+
+			mu.Unlock()
+		})
+
 		body["type"] = "topology_ok"
 
 		delete(body, "topology")
+
+		if isMsgFromNode {
+			return nil
+		}
+
 		return n.Reply(msg, body)
+	})
+
+	n.Handle("gossip", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		gVal := body["gossip_val"].(float64)
+		gUid := body["gossip_uid"].(string)
+
+		mu.Lock()
+		if _, ok := seenGossipUid[gUid]; ok {
+			mu.Unlock()
+			return nil
+		}
+
+		seenGossipUid[gUid] = struct{}{}
+		arr = append(arr, gVal)
+		mu.Unlock()
+
+		for _, nxt := range next {
+			n.Send(nxt, body)
+		}
+
+		return nil
 	})
 
 	log.Println("Server running...")
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func SendNewGossip(n *maelstrom.Node, next []string, val float64) {
+	gossipBody := make(map[string]any)
+	gossipBody["type"] = "gossip"
+	gossipBody["gossip_uid"] = rand.Text()
+	gossipBody["gossip_val"] = val
+	for _, nxt := range next {
+		n.Send(nxt, gossipBody)
 	}
 }
