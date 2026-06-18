a5b0975fa164922474f5d47f73dbc1cba66d169b
Sahil Patil <sp92535@gmail.com>
2026-06-18 18:52:47 +0000

    Added challenge #03: Broadcast [#3d & #3e done]

--- cmd/broadcast/main.go
+++ cmd/broadcast/main.go
@@ -1,45 +1,105 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/hellinvoid/dist-sys-flyio/internal/snowflake"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+const (
+	POLL_INTERVAL = 100 * time.Millisecond
+	POLL_TIMEOUT  = 700 * time.Millisecond
+)
+
+type Entry struct {
+	UId uint64
+	Val float64
+}
+
 func main() {
+
+	n := maelstrom.NewNode()
+
 	idgen := snowflake.NewIdGenerator()
 
 	var once sync.Once
-
 	var mu sync.Mutex
-	arr := make([]float64, 0)
+	entries := make([]Entry, 0)
 
-	n := maelstrom.NewNode()
-	next := make([]string, 0)
+	var seenUid sync.Map
 
-	seenGossipUid := map[float64]any{}
+	startPoll := func(n *maelstrom.Node, nxt string) {
+		body := make(map[string]any)
+		body["type"] = "poll"
+
+		var offset atomic.Int64
+
+		pollResponseHandler := func(msg maelstrom.Message) error {
+			var res map[string]any
+			if err := json.Unmarshal(msg.Body, &res); err != nil {
+				return err
+			}
+
+			arr := res["entries"].([]any)
+			offset.Add(int64(len(arr)))
+
+			for _, obj := range arr {
+				eMap := obj.(map[string]any)
+				e := Entry{
+					UId: uint64(eMap["UId"].(float64)),
+					Val: eMap["Val"].(float64),
+				}
+
+				if _, ok := seenUid.LoadOrStore(e.UId, struct{}{}); !ok {
+					addEntry(&entries, e, &mu)
+				}
+
+			}
+
+			return nil
+		}
+
+		ticker := time.NewTicker(POLL_INTERVAL)
+
+		for range ticker.C {
+			body["offset"] = offset.Load()
+			ctx, cancel := context.WithTimeout(context.Background(), POLL_TIMEOUT)
+			msg, err := n.SyncRPC(ctx, nxt, body)
+			cancel()
+			if err != nil {
+				continue
+			}
+			pollResponseHandler(msg)
+		}
+
+	}
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		go n.Reply(msg, map[string]any{
+			"type": "broadcast_ok",
+		})
 
 		val := body["message"].(float64)
 
-		mu.Lock()
-		arr = append(arr, val)
-		mu.Unlock()
+		e := Entry{
+			UId: idgen.GetUniqueId(n.ID()),
+			Val: val,
+		}
 
-		go SendNewGossip(n, next, val, idgen)
+		seenUid.Store(e.UId, struct{}{})
+		addEntry(&entries, e, &mu)
 
-		body["type"] = "broadcast_ok"
-
-		delete(body, "message")
-		return n.Reply(msg, body)
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -47,10 +107,14 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-
 		body["type"] = "read_ok"
+
+		msgs := make([]float64, 0)
+
 		mu.Lock()
-		msgs := append([]float64(nil), arr...)
+		for _, e := range entries {
+			msgs = append(msgs, e.Val)
+		}
 		mu.Unlock()
 
 		body["messages"] = msgs
@@ -64,63 +128,40 @@ func main() {
 			return err
 		}
 
-		isMsgFromNode := msg.Src[0] == 'n'
-
-		once.Do(func() {
-			mu.Lock()
-
-			neighbours := body["topology"].(map[string]any)[n.ID()].([]any)
-			for _, n := range neighbours {
-				next = append(next, n.(string))
-			}
-
-			for _, nxt := range next {
-				if msg.Src != nxt {
-					n.Send(nxt, body)
-				}
-			}
-			for _, val := range arr {
-				go SendNewGossip(n, next, val, idgen)
-			}
-
-			mu.Unlock()
-		})
-
-		body["type"] = "topology_ok"
-
-		delete(body, "topology")
-
-		if isMsgFromNode {
-			return nil
+		if msg.Src[0] != 'n' {
+			go n.Reply(msg, map[string]any{
+				"type": "topology_ok",
+			})
 		}
 
-		return n.Reply(msg, body)
+		neighbours := body["topology"].(map[string]any)[n.ID()].([]any)
+		once.Do(func() {
+			for _, neigh := range neighbours {
+				nxt := neigh.(string)
+				n.Send(nxt, body)
+				go startPoll(n, nxt)
+			}
+		})
+
+		return nil
 	})
 
-	n.Handle("gossip", func(msg maelstrom.Message) error {
+	n.Handle("poll", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		body["type"] = "poll_ok"
 
-		gVal := body["gossip_val"].(float64)
-		gUid := body["gossip_uid"].(float64)
+		offset := int(body["offset"].(float64))
 
-		mu.Lock()
-		if _, ok := seenGossipUid[gUid]; ok {
-			mu.Unlock()
-			return nil
+		if offset < entriesLen(&entries, &mu) {
+			body["entries"] = entries[offset:]
+		} else {
+			body["entries"] = []Entry{}
 		}
 
-		seenGossipUid[gUid] = struct{}{}
-		arr = append(arr, gVal)
-		mu.Unlock()
-
-		for _, nxt := range next {
-			n.Send(nxt, body)
-		}
-
-		return nil
+		return n.Reply(msg, body)
 	})
 
 	log.Println("Server running...")
@@ -129,12 +170,15 @@ func main() {
 	}
 }
 
-func SendNewGossip(n *maelstrom.Node, next []string, val float64, idgen *snowflake.IdGenerator) {
-	gossipBody := make(map[string]any)
-	gossipBody["type"] = "gossip"
-	gossipBody["gossip_uid"] = idgen.GetUniqueId(n.ID())
-	gossipBody["gossip_val"] = val
-	for _, nxt := range next {
-		n.Send(nxt, gossipBody)
-	}
+func addEntry(arr *[]Entry, e Entry, mu *sync.Mutex) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	*arr = append(*arr, e)
+}
+
+func entriesLen(arr *[]Entry, mu *sync.Mutex) int {
+	mu.Lock()
+	defer mu.Unlock()
+	return len(*arr)
 }
